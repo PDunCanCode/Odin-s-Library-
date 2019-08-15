684224a9fb7f86723e19790c3138a39598194130
Paus Haus <48162083+PDunCanCode@users.noreply.github.com>
2019-08-15 14:06:00 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -14,3 +14,18 @@ class Book {
   
       books.forEach((book) => UI.addBookToList(book));
     }
+  static addBookToList(book) {
+      const list = document.querySelector('#book-list');
+
+      const row = document.createElement('tr');
+
+      row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td> <a href="#" class="btn btn-danger btn-sm delete">X</td>
+        `;
+
+        list.appendChild(row);
+  }
+  }
