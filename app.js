f30cc450e76634e217dfb1dc88e4fe32e1eb901a
Paus Haus <48162083+PDunCanCode@users.noreply.github.com>
2019-08-15 11:32:34 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+// Book Class: Represents a Book
+class Book {
+    constructor(title, author, isbn) {
+      this.title = title;
+      this.author = author;
+      this.isbn = isbn;
+    }
+  }
+  
+  // UI Class: Handle UI Tasks
+  class UI {
+    static displayBooks() {
+      const books = Store.getBooks();
+  
+      books.forEach((book) => UI.addBookToList(book));
+    }
