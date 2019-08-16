0e1a4352d97515c31e10cdf54c78b21e6c8cc9fa
Paus Haus <48162083+PDunCanCode@users.noreply.github.com>
2019-08-15 19:20:04 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -100,4 +100,35 @@ class Book {
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
   
-  }
+    // Validate
+    if(title === '' || author === '' || isbn === '') {
+      UI.showAlert('Please fill in all fields', 'danger');
+    } else {
+      // Instatiate book
+      const book = new Book(title, author, isbn);
+  
+      // Add Book to UI
+      UI.addBookToList(book);
+  
+      // Add book to store
+      Store.addBook(book);
+  
+      // Show success message
+      UI.showAlert('Book Added', 'success');
+  
+      // Clear fields
+      UI.clearFields();
+    }
+  });
+  
+  // Event: Remove a Book
+  document.querySelector('#book-list').addEventListener('click', (e) => {
+    // Remove book from UI
+    UI.deleteBook(e.target);
+  
+    // Remove book from store
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+  
+    // Show success message
+    UI.showAlert('Book Removed', 'success');
+  });
